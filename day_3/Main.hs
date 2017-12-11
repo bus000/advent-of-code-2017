902c764208177b51d3aa146047b75cc5d2a5ee32
Magnus Stavngaard <magnus@stavngaard.dk>
2017-12-11 23:29:11 +0100

    Implement day 3

---
+++ day_3/Main.hs
@@ -0,0 +1,28 @@
+module Main (main) where
+
+import Prelude hiding (Either(..))
+
+type SpiralCoordinate = Int
+type EuclideanCoordinate = (Int, Int)
+
+data Direction = Up | Down | Left | Right
+
+main :: IO ()
+main = do
+    n <- read . init <$> getContents :: IO Int
+
+    let (x, y) = toEuclidean n
+
+    print $ abs x + abs y
+
+toEuclidean :: SpiralCoordinate -> EuclideanCoordinate
+toEuclidean n = foldr move (0, 0) (take (n - 1) movements)
+  where
+    movements = concat $ zipWith replicate lengths directions
+    lengths = concatMap (replicate 2) [1..]
+    directions = cycle [Right, Up, Left, Down]
+
+    move Right (x, y) = (x + 1, y)
+    move Up (x, y) = (x, y + 1)
+    move Left (x, y) = (x - 1, y)
+    move Down (x, y) = (x, y - 1)
