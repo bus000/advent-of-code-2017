8fd171c83e1f4e9460f8d3cacd5549b5e0272f7e
Magnus Stavngaard <magnus@stavngaard.dk>
2017-12-11 22:35:10 +0100

    Implement day 1

---
+++ day_1/Main.hs
@@ -0,0 +1,23 @@
+module Main (main) where
+
+import Data.Semigroup ((<>))
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+-- Sum of all digits matching the next.
+-- List is circular.
+
+main :: IO ()
+main = do
+    (first, rest) <- T.splitAt 1 . T.init <$> T.getContents
+
+    let input = first <> rest <> first
+        toSum
+            = map read
+            . map (:[])
+            . T.unpack
+            . T.concat
+            . map (T.drop 1)
+            $ T.group input
+
+    print $ sum toSum
