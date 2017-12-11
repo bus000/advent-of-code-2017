dbd913ee90d597c98face78ed0139e4e94aa158b
Magnus Stavngaard <magnus@stavngaard.dk>
2017-12-11 23:44:41 +0100

    Implement day 4

---
+++ day_4/Main.hs
@@ -0,0 +1,12 @@
+module Main (main) where
+
+import Data.List (group, sort)
+
+main :: IO ()
+main = do
+    valid <- filter validpass . lines <$> getContents
+
+    print $ length valid
+
+validpass :: String -> Bool
+validpass = not . any (> 1) . map length . group . sort . words
