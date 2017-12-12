26a17ff168a7672dad9304b5bc398224d3482247
Magnus Stavngaard <magnus@stavngaard.dk>
2017-12-12 13:35:32 +0100

    Implement day 5

---
+++ day_5/Main.hs
@@ -0,0 +1,36 @@
+module Main (main) where
+
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict ((!))
+import qualified Control.Monad.State as S
+import qualified Control.Monad.Loops as C
+import Data.Maybe (isNothing)
+
+type Position = Int
+type Jump = Int
+type Finished = Bool
+
+data CPU = CPU (Map.Map Position Jump) Position deriving Show
+
+main :: IO ()
+main = do
+    ns <- map read . lines <$> getContents
+
+    let cpu = CPU (Map.fromList $ zip [0..] ns) 0
+
+    print $ moveAll cpu
+
+moveAll :: CPU -> Int
+moveAll initState =
+    length $ S.evalState (C.untilM move (isNothing <$> getVal)) initState
+
+move :: S.State CPU ()
+move = do
+    (CPU jumps pos) <- S.get
+    let jump = jumps ! pos
+    S.put $ CPU (Map.insert pos (jump + 1) jumps) (pos + jump)
+
+getVal :: S.State CPU (Maybe Int)
+getVal = do
+    (CPU jumps pos) <- S.get
+    return $ Map.lookup pos jumps
