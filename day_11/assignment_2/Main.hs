1c4a50101a9d31fc21596a1162e03d9d2d55d32f
Magnus Stavngaard <magnus@stavngaard.dk>
2017-12-17 22:40:45 +0100

    Implement day 11 assignment 2

---
+++ day_11/assignment_2/Main.hs
@@ -0,0 +1,33 @@
+module Main (main) where
+
+import qualified Data.List.Split as L
+import qualified Data.Char as Char
+
+data Direction = N | NE | SE | S | SW | NW deriving (Show, Read)
+
+type Position = (Int, Int)
+
+main :: IO ()
+main = do
+    input <- map read . L.splitOn "," . map Char.toUpper <$> getContents
+
+    let positions = allPos (0, 0) input
+        distances = map (distance (0, 0)) positions
+
+    print $ maximum distances
+
+allPos :: Position -> [Direction] -> [Position]
+allPos start = scanl move start
+  where
+    move (x, y) N = (x + 1, y + 0)
+    move (x, y) NE = (x + 1, y - 1)
+    move (x, y) SE = (x + 0, y - 1)
+    move (x, y) S = (x - 1, y + 0)
+    move (x, y) SW = (x - 1, y + 1)
+    move (x, y) NW = (x + 0, y + 1)
+
+distance :: Position -> Position -> Int
+distance (x1, y1) (x2, y2) = maximum $ map abs [x2 - x1, y2 - y1, z2 - z1]
+  where
+    z1 = -(x1 + y1)
+    z2 = -(x2 + y2)
