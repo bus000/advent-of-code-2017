1045c567043b332617775000c2ab1208b0537498
Magnus Stavngaard <magnus@stavngaard.dk>
2017-12-20 07:27:38 +0100

    Implement day 14 assignment 1

---
+++ day_14/assignment_1/Main.hs
@@ -0,0 +1,32 @@
+module Main (main) where
+
+import qualified KnotHash as KH
+
+main :: IO ()
+main = do
+    input <- init <$> getContents
+
+    let strings = map (\x -> input ++ "-" ++ show x) [0..127]
+        hash = concatMap KH.knothash strings
+        bits = concatMap hexVal hash
+
+    print $ length (filter id bits)
+
+hexVal :: Char -> [Bool]
+hexVal '0' = [False, False, False, False]
+hexVal '1' = [False, False, False, True ]
+hexVal '2' = [False, False, True , False]
+hexVal '3' = [False, False, True , True ]
+hexVal '4' = [False, True , False, False]
+hexVal '5' = [False, True , False, True ]
+hexVal '6' = [False, True , True , False]
+hexVal '7' = [False, True , True , True ]
+hexVal '8' = [True , False, False, False]
+hexVal '9' = [True , False, False, True ]
+hexVal 'a' = [True , False, True , False]
+hexVal 'b' = [True , False, True , True ]
+hexVal 'c' = [True , True , False, False]
+hexVal 'd' = [True , True , False, True ]
+hexVal 'e' = [True , True , True , False]
+hexVal 'f' = [True , True , True , True ]
+hexVal _ = error "Not hex"
