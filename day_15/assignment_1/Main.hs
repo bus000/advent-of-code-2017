3ff5c14f53a7621adf78c87bfe231acd62383486
Magnus Stavngaard <magnus@stavngaard.dk>
2017-12-23 10:36:36 +0100

    Implement day 15 assignment 1

---
+++ day_15/assignment_1/Main.hs
@@ -0,0 +1,22 @@
+module Main (main) where
+
+import qualified Data.Bits as Bits
+import qualified Data.Int as Int
+
+main :: IO ()
+main = do
+    [gena, genb] <- map (read . last . words) . lines <$> getContents
+
+    print $ length . filter compareLowest . take 40000000 $ anbs gena genb
+
+compareLowest :: (Int.Int32, Int.Int32) -> Bool
+compareLowest (a, b) = Bits.shift a 16 == Bits.shift b 16
+
+anbs :: Integer -> Integer -> [(Int.Int32, Int.Int32)]
+anbs a b = zip (map fromIntegral $ as a) (map fromIntegral $ bs b)
+
+as :: Integer -> [Integer]
+as = drop 1 . iterate (\x -> (x * 16807) `mod` 2147483647)
+
+bs :: Integer -> [Integer]
+bs = drop 1 . iterate (\x -> (x * 48271) `mod` 2147483647)
