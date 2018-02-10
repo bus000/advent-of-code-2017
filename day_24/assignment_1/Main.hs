e340ef78f078ccf4e9ccc9d25645911440ad1a21
Magnus Stavngaard <magnus@stavngaard.dk>
2018-02-10 17:03:54 +0100

    Finish day 24 assignment 1

--- day_24/assignment_1/Main.hs
+++ day_24/assignment_1/Main.hs
@@ -2,47 +2,30 @@
 module Main (main) where
 
 import ClassyPrelude
-import qualified Data.List as L
-import qualified Data.List.Extra as L
 import qualified Data.Text.Lazy as T
 import qualified Data.Word as Word
 import Prelude ()
 import qualified Data.Vector as V
-import qualified Data.Map.Lazy as Map
 
 data Link = Link { _start :: !Word.Word32, _end :: !Word.Word32 }
   deriving (Show, Eq)
-
-data Tree a = Node a [Tree a] deriving (Show)
 
 main :: IO ()
 main = do
     links <- parseInput <$> getContents
 
-    print $ map maxPath $ buildTree 0 $ buildLinkMap links
-    {-mapM_ print $ map allPaths $ buildTree 0 $ buildLinkMap links-}
+    print . maxPath (Link 0 0) . V.fromList $ links
 
-buildLinkMap :: [Link] -> Map Word.Word32 (V.Vector Link)
-buildLinkMap links = Map.fromAscList $ zip mapkeys mapvals
-  where
-    mapkeys = L.nub . sort . map _start $ links
-    mapvals = map V.fromList . L.groupOn _start . L.sortOn _start $ links
-
-buildTree :: Word.Word32 -> Map.Map Word.Word32 (V.Vector Link) -> [Tree Link]
-buildTree n links = map consTree legalLinks
-  where
-    legalLinks = extractAll $ maybe V.empty id (Map.lookup n links)
-    consTree (l@(Link _ e), otherlinks) =
-        Node l $ buildTree e (Map.insert n otherlinks links)
-
-maxPath :: Tree Link -> Word.Word32
-maxPath (Node (Link s e) ts) = case fromNullable (map maxPath ts) of
+maxPath :: Link -> V.Vector Link -> Word.Word32
+maxPath (Link s e) links = case fromNullable (map (uncurry maxPath) legalLinks) of
     Nothing -> s + e
-    Just ts' -> s + e + maximum ts'
-
-allPaths :: Tree Link -> [[Link]]
-allPaths (Node l []) = [[l]]
-allPaths (Node l ls) = map (l:) (concatMap allPaths ls)
+    Just legals -> s + e + maximum legals
+  where
+    legalLinks = filter legalNext . map flipLegal $ extractAll links
+    legalNext (Link s' e', _) = s' == e || e' == e
+    flipLegal (Link s' e', vs)
+        | s' /= e = (Link e' s', vs)
+        | otherwise = (Link s' e', vs)
 
 parseInput :: LText -> [Link]
 parseInput = mapMaybe bridge . lines
